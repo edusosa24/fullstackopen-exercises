079b76fa680b135a434c3ce5097cb6cb0bc9d68b
Eduardo Sosa <sm.edu33@gmail.com>
2023-12-13 15:24:18 -0300

    Part 2 Exercise 8

--- Part 2/phonebook/src/index.js
+++ Part 2/phonebook/src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom';
 import { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '40-20-565620' },
+  ]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
   const handleNameChange = (event) => {
     event.preventDefault();
     setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    event.preventDefault();
+    setNewNumber(event.target.value);
   };
 
   const isNoDuplicate = () => {
@@ -21,11 +29,12 @@ const App = () => {
     return isValid;
   };
 
-  const handleNewName = (event) => {
+  const handleNewPerson = (event) => {
     event.preventDefault();
     const isValid = isNoDuplicate();
     if (isValid) {
-      setPersons(persons.concat({ name: newName }));
+      setPersons(persons.concat({ name: newName, number: newNumber }));
+      setNewNumber('');
       setNewName('');
     } else {
       alert(`${newName} is already on the phonebook.`);
@@ -38,9 +47,11 @@ const App = () => {
       <form>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
+          <br />
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
-          <button type="submit" onClick={handleNewName}>
+          <button type="submit" onClick={handleNewPerson}>
             add
           </button>
         </div>
@@ -49,7 +60,9 @@ const App = () => {
       <div>
         debug:{' '}
         {persons.map((person) => (
-          <p key={person.name}>{person.name}</p>
+          <p key={person.name}>
+            {person.name}, {person.number}
+          </p>
         ))}
       </div>
     </div>
