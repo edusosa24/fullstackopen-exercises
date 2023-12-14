a525606ec8ad2836b1aac9369cee0cf991844464
Eduardo Sosa <sm.edu33@gmail.com>
2023-12-14 16:33:50 -0300

    Part 2 Exercise 11

--- Part 2/phonebook/src/index.js
+++ Part 2/phonebook/src/index.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { useState } from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import { AddContact } from './components/AddContact';
 import { FilterByName } from './components/FilterByName';
 import { ContactsDisplay } from './components/DisplayContacts';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filterName, setFilterName] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   return (
     <>
